f23bbc8bee29348b554eb8a4eb308a12b3077971
Charles Erickson <cse621@gmail.com>
2019-04-09 11:11:16 -0500

    Built several tests for the remove method and the string method

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -8,9 +8,18 @@ namespace CustomList
 {
    public class CustomList<T>
     {
-        private T[]=new T[1];
+        private T[]items =new T[1];
 
         public void Add(T item)
+        {
+
+        }
+
+        public void Remove(T item)
+        {
+
+        }
+        public string ToString()
         {
 
         }
--- CustomList/UnitTest1.cs
+++ CustomList/UnitTest1.cs
@@ -8,22 +8,109 @@ namespace CustomListTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_Of_Add()
         {
             //Arrange
-            CustomList<int> MyList = new CustomList<int>;
+            CustomList<int> myList = new CustomList<int>;
             int value = 5;
 
 
             //Act
-            MyList.Add(value);
+            myList.Add(value);
 
 
 
 
             //Assert
-            Assert.AreEqual(value, MyList[0]);
+            Assert.AreEqual(value, myList[0]);
 
+        }
+        [TestMethod]
+        public void Test_Of_Count()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value = 5;
+            int expectedCount = 1;
+            //Act
+            myList.Add(value);
+
+            //Assert
+            Assert.AreEqual(expectedCount, myList.Count);
+        }
+        [TestMethod]
+        public void Test_of_Add_Method_With_Multiple_Values()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value = 5;
+            int value1 = 10;
+
+            //Act
+            myList.Add(value);
+            myList.Add(value1);
+
+            //Assert
+            Assert.AreEqual(value, myList[0]);
+        }
+
+        [TestMethod]
+        public void Test_Of_Remove()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value = 5;
+            int value2 = 4;
+            int expectedCount = 1;
+
+            //Act
+            myList.Add(value);
+            myList.Add(value2);
+            myList.Remove(value);
+
+
+
+            //Assert
+            Assert.AreEqual(expectedCount, myList.Count);            
+        }
+
+        [TestMethod]
+        public void Test_Of_Remove_Checking_Proper_()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value = 5;
+            int value2 = 4;
+            int expectedValue = 4;
+
+            //Act
+            myList.Add(value);
+            myList.Add(value2);
+            myList.Remove(value);
+
+
+
+            //Assert
+            Assert.AreEqual(expectedValue, myList[0});
+        }
+
+        [TestMethod]
+        public void Test_Of_List_To_string()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value = 5;
+            int value2 = 4;
+            string expectedString = "54";
+            string name;
+
+            //Act
+            myList.Add(value);
+            myList.Add(value2);
+            name = myList.ToString();
+
+            //Assert
+            Assert.AreEqual(expectedString, name);
         }
     }
 }
